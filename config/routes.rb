ae8b6d790cd7e6e6fd777933e25207e6d1bd4563
Evgenia Manolova <emanolova@gmail.com>
2017-12-08 22:02:11 +0200

    Configure application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tweets
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tweets, only: :index
+
+  resources :users, only: [:index, :show, :edit, :update, :destroy] do
+    resources :tweets
+  end
+
+  root to: "tweets#index"
 end
